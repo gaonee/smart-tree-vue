43a50959ca3d3f7ef866e1f0b1870b494bc95926
Gaonee <841374169@qq.com>
2019-09-06 01:05:29 +0800

    clean

--- packages/tree/index.ts
+++
@@ -1,7 +0,0 @@
-import Tree from './src/tree.vue';
-
-(Tree as any).install = (Vue: any) => {
-    Vue.component(Tree.name, Tree);
-}
-
-export default Tree;
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './smart-tree'
-
-import * as SmartTree from './smart-tree'
-export default SmartTree
