a5c9354721bbc60bb1a161bd459843d421120e76
gaonee <majiancommon@163.com>
2019-10-21 19:48:35 +0800

    Update types.ts

--- packages/tree/src/model/types.ts
+++ packages/tree/src/model/types.ts
@@ -1,4 +1,19 @@
 export interface NodeData {
     label: string
-    children?: NodeData
+    children?: NodeData[]
+    id?: string
+    pId?: string
+}
+
+export interface TreeProps {
+    label: string
+    children: string
+}
+
+export interface StoreProps {
+    props: TreeProps
+    nodeKey: string
+    defaultExpandLevel: number
+    simple: boolean
+    filterNodeMethod?: Function
 }
